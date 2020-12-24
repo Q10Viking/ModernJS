e99b69537a033b3933ec57b2f068e990237bf4fa
zhuangzhuang-huang <cau1403090523@gmail.com>
2020-12-24 10:49:28 +0800

    complete easy http library

--- 02 Asynchronous/04 Make EasyHttp library/app.js
+++ 02 Asynchronous/04 Make EasyHttp library/app.js
@@ -1,9 +1,27 @@
 const http = new EasyHttp();
 
-http.get('https://jsonplaceholder.typicode.com/posts',function(error,posts){
-    if(error){
-        console.error(error);
-    }else{
-        console.log(posts);
-    }
+// http.get('https://jsonplaceholder.typicode.com/posts',function(error,posts){
+//     if(error){
+//         console.error(error);
+//     }else{
+//         console.log(posts);
+//     }
+// })
+
+let data = {
+    title: 'test',
+    body: 'Hello World'
+}
+
+// http.post('https://jsonplaceholder.typicode.com/posts',data,function(error,response){
+//     console.log(response);
+// })
+
+// http.put('https://jsonplaceholder.typicode.com/posts/1',data,function(error,response){
+//     console.log(response);
+// })
+
+http.delete('https://jsonplaceholder.typicode.com/posts/1',function(error,response){
+    console.log(response);
 })
+
--- 02 Asynchronous/04 Make EasyHttp library/easyHttp.js
+++ 02 Asynchronous/04 Make EasyHttp library/easyHttp.js
@@ -15,3 +15,46 @@ EasyHttp.prototype.get = function(url,callback){
     }
     this.http.send();
 }
+
+
+//  POST Method
+EasyHttp.prototype.post = function(url,data,callback){
+    this.http.open('POST',url);
+    this.http.setRequestHeader("Content-Type", "application/json");
+    let that = this;
+
+    this.http.onload = function(){
+        // console.log(that.http.status); 201
+        callback(null,that.http.responseText);
+    }
+
+    this.http.send(JSON.stringify(data));
+}
+
+EasyHttp.prototype.put = function(url,data,callback){
+    this.http.open('PUT',url);
+    this.http.setRequestHeader("Content-Type", "application/json");
+    let that = this;
+
+    this.http.onload = function(){
+        // console.log(that.http.status); 201
+        callback(null,that.http.responseText);
+    }
+
+    this.http.send(JSON.stringify(data));
+}
+
+
+//  GET Method
+EasyHttp.prototype.delete = function(url,callback){
+    this.http.open('GET',url);
+    let that = this;
+    this.http.onload = function(){
+        let obj = {
+            status: 'success',
+            result: that.http.responseText
+        }
+        callback(null,JSON.stringify(obj));
+    }
+    this.http.send();
+}
